baf0b098b6fb116b9c7f197ac40c59e44e0b242f
alexander.oliveira <alexander.oliveira99@gmail.com>
2024-09-30 21:01:25 +0100

    I was undestarding about standalone concepts vs module

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Rigth now, I am learning Angular 12 concepts
 
 # Angular comands
-Module !important
+Module - !important
 ng g m app
--- helpdesk-app/src/app/app.component.ts
+++ helpdesk-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [MatButtonModule], 
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
