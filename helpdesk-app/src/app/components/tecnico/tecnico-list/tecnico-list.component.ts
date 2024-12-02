18a9ebcbf8b8c24966ce00c78a6ad3e03097c786
alexander1914 <alexander.oliveira99@gmail.com>
2024-12-02 21:09:33 +0000

    I was cleaning code this tecnico component

--- helpdesk-app/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ helpdesk-app/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -35,9 +35,7 @@ export class TecnicoListComponent {
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  ngAfterViewInit(): void {
-    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
-    //Add 'implements AfterViewInit' to the class.
+  ngAfterViewInit(): void {    
     this.dataSource.paginator = this.paginator    
   }
 }
