86aafd2a49284b811f9977c7c737497e85c91c2a
alexander1914 <alexander.oliveira99@gmail.com>
2025-12-11 20:13:11 +0000

    [Login Component] - I created new login component and install angular material today

---
+++ helpdesk-app/src/app/components/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
