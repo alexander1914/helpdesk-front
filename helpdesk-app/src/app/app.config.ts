ed669e43f37ef61a0f93d9263851163996badf94
alexander1914 <alexander.oliveira99@gmail.com>
2025-12-29 18:49:58 +0000

    [Login Component] - I implemented some functions to login changed layout of project and install toastr messages today

--- helpdesk-app/src/app/app.config.ts
+++ helpdesk-app/src/app/app.config.ts
@@ -3,7 +3,17 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+    provideToastr({
+      timeOut: 4000,
+      closeButton: true,
+      progressBar: true
+    })
+  ]
 };
--- helpdesk-app/src/app/components/login/login.component.ts
+++ helpdesk-app/src/app/components/login/login.component.ts
@@ -1,16 +1,46 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Credenciais } from '../../models/credenciais';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { timeout } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  creds: Credenciais = {
+    email: '',
+    password: ''
+  }
+
+  emailControl = new FormControl(null, Validators.email);
+  passwordControl = new FormControl(null, Validators.minLength(3))
+
+  constructor(private toast: ToastrService) { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  validateFields(): boolean {
+    if (this.emailControl.valid && this.passwordControl.valid) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  login(){
+    this.toast.error('User and or password inválid', 'Login');
+    this.creds.password = '';
+  }
 
 }
