df776588f9f8c0858e358f1928d8cedba19c29b7
alexander1914 <alexander.oliveira99@gmail.com>
2026-02-23 22:19:40 +0000

    [Clientte Components] - I had been creating all components of cliente, model and a service today

---
+++ helpdesk-app/src/app/components/cliente/cliente-delete/cliente-delete.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-delete',
+  standalone: true,
+  imports: [],
+  templateUrl: './cliente-delete.component.html',
+  styleUrl: './cliente-delete.component.css'
+})
+export class ClienteDeleteComponent {
+
+}
