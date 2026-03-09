794d2014e0403908adab0a06e1e7fffe37595410
alexander1914 <alexander.oliveira99@gmail.com>
2026-03-09 18:23:32 +0000

    [Chamados Component] - I was creating new chamado-create component today

--- helpdesk-app/src/app/app.routes.ts
+++ helpdesk-app/src/app/app.routes.ts
@@ -16,6 +16,7 @@ import { ClienteUpdateComponent } from './components/cliente/cliente-update/clie
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
 
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
+import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
 
 export const routes: Routes = [
     { path: "login", component: LoginComponent },
@@ -33,7 +34,8 @@ export const routes: Routes = [
             { path: "clientes/update/:id", component: ClienteUpdateComponent },
             { path: "clientes/delete/:id", component: ClienteDeleteComponent },
 
-            { path: "chamados", component: ChamadoListComponent }
+            { path: "chamados", component: ChamadoListComponent },
+            { path: "chamados/create", component: ChamadoCreateComponent }
         ]
     }
 ];
---
+++ helpdesk-app/src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-chamado-create',
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
+  templateUrl: './chamado-create.component.html',
+  styleUrl: './chamado-create.component.css'
+})
+export class ChamadoCreateComponent {
+
+}
