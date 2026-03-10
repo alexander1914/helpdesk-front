c41d85bd72fdc5bd7af04bddfe1da7cd5a9bed47
alexander1914 <alexander.oliveira99@gmail.com>
2026-03-10 20:53:07 +0000

    [Chamados Componente] - I implmented the validations to chamado-create component today

--- helpdesk-app/src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ helpdesk-app/src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -8,10 +8,29 @@ import { MatSelectModule } from '@angular/material/select';
 @Component({
   selector: 'app-chamado-create',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './chamado-create.component.html',
   styleUrl: './chamado-create.component.css'
 })
-export class ChamadoCreateComponent {
+export class ChamadoCreateComponent implements OnInit {
+
+  priority: FormControl = new FormControl(null, [Validators.required]);
+  status: FormControl = new FormControl(null, [Validators.required]);
+  titulo: FormControl = new FormControl(null, [Validators.required]);
+  observation: FormControl = new FormControl(null, [Validators.required]);
+  technical: FormControl = new FormControl(null, [Validators.required]);
+  customer: FormControl = new FormControl(null, [Validators.required]);
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  validateFields(): boolean {
+    return this.priority.valid && this.status.valid &&
+      this.titulo.valid && this.observation.valid &&
+      this.technical.valid && this.customer.valid;
+  }
 
 }
